dcec5362006d2bb00d6dc0e1074f3d6323433b31
GOD-Da <dawiniefleury17@gmail.com>
2025-11-02 04:02:11 -0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'goatvillano480@gmail.com',   //use your real vaild mega account email
-    password: 'Lens122&@',  ////use your real vaild mega account password
+    email: 'clay_exurban.6l@icloud.com',   //use your real vaild mega account email
+    password: 'Jetne7-javkuz-nofnyr',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
